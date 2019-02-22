61734ff53c340a639b186e3fd4e32f69e5696f83
gobinda <gobinda.paul.1547@gmail.com>
2019-02-23 04:34:39 +0600

    selecting the date only

--- app/src/main/java/org/office/time/MainActivity.java
+++ app/src/main/java/org/office/time/MainActivity.java
@@ -1,21 +1,15 @@
 package org.office.time;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
-import android.widget.TimePicker;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DEBUG = "[MainActivity]";
 
-    private View dialogView ;
-    private AlertDialog alertDialog;
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,33 +18,16 @@ public class MainActivity extends AppCompatActivity {
         Log.d(DEBUG, "onCreateCalled");
 
 
-        dialogView = View.inflate(this, R.layout.date_time_picker, null);
-
-        alertDialog  = new AlertDialog.Builder(this).create();
-        dialogView.findViewById(R.id.date_time_set).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.date_picker);
-                TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.time_picker);
-
-                Calendar calendar = new GregorianCalendar(datePicker.getYear(),
-                        datePicker.getMonth(),
-                        datePicker.getDayOfMonth(),
-                        timePicker.getCurrentHour(),
-                        timePicker.getCurrentMinute());
-
-                long time = calendar.getTimeInMillis();
-                Log.d(DEBUG, "time = " + time);
-                alertDialog.dismiss();
-            }});
+        datePicker = findViewById(R.id.date_picker);
 
     }
 
-    public void showDateTimePicker(View v){
-        Log.d(DEBUG, "showing date time picker");
-        alertDialog.setView(dialogView);
-        alertDialog.show();
+    public void dateSelectionFinal(View v){
+        Log.d(DEBUG, "dateSelectionFinal");
+
+        Log.d(DEBUG, String.valueOf(datePicker.getYear()));
+        Log.d(DEBUG, String.valueOf(datePicker.getMonth()));
+        Log.d(DEBUG, String.valueOf(datePicker.getDayOfMonth()));
     }
 
 }
