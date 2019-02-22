2978a81c188751e9ac613416314197d71fb357f3
shimulcse22 <shimul.cse22@gmail.com>
2019-02-23 04:15:28 +0600

    Added log line in on create method

--- app/src/main/java/org/office/time/MainActivity.java
+++ app/src/main/java/org/office/time/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(DEBUG, "onCreateCalled");
+
 
         dialogView = View.inflate(this, R.layout.date_time_picker, null);
 
