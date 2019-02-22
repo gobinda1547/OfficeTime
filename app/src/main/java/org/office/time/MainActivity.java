985d5a0832449135a0a249761bd903844508a317
gobinda <gobinda.paul.1547@gmail.com>
2019-02-23 03:39:17 +0600

    checked the date time picker

--- app/src/main/java/org/office/time/MainActivity.java
+++ app/src/main/java/org/office/time/MainActivity.java
@@ -1,14 +1,53 @@
 package org.office.time;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String DEBUG = "[MainActivity]";
+
+    private View dialogView ;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialogView = View.inflate(this, R.layout.date_time_picker, null);
+
+        alertDialog  = new AlertDialog.Builder(this).create();
+        dialogView.findViewById(R.id.date_time_set).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.date_picker);
+                TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.time_picker);
+
+                Calendar calendar = new GregorianCalendar(datePicker.getYear(),
+                        datePicker.getMonth(),
+                        datePicker.getDayOfMonth(),
+                        timePicker.getCurrentHour(),
+                        timePicker.getCurrentMinute());
+
+                long time = calendar.getTimeInMillis();
+                Log.d(DEBUG, "time = " + time);
+                alertDialog.dismiss();
+            }});
+
+    }
+
+    public void showDateTimePicker(View v){
+        alertDialog.setView(dialogView);
+        alertDialog.show();
     }
 
 }
